96770121f0a1655e7c413dacd18c11b8fd605626
fnery <fabiornery@gmail.com>
2020-04-28 16:09:15 +0100

    MNT: testing pep8speaks after PR#167

---
+++ pep8speaks_test.py
@@ -0,0 +1,6 @@
+"""
+This file intentionallly violates pep8 to test the pep8speaks bot
+Should trigger long line, no space after comma and final newline missing
+"""
+import numpy as np
+A_VERY_LONG_LINE = np.array([[0,0,0,1], [0, 0, 0, 1], [0, 0, 0, 1], [0, 0, 0, 1]]) + np.array([[0, 0, 0, 1], [0, 0, 0, 1], [0, 0, 0, 1], [0, 0, 0, 1]])
