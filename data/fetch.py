c4dc7786643f38851eaa93a8aaae52cfa43fb3c5
João Almeida e Sousa <53518061+JSousa-UoL@users.noreply.github.com>
2020-06-05 20:25:10 +0100

    Data fetch for B0 GE and B0 SIEMENS and corresponding Jupyter Notebooks. Real/Imaginary to Phase included.

--- data/fetch.py
+++ data/fetch.py
@@ -169,6 +169,227 @@ def r2star_ge():
     # Sort by increasing echo time
     sort_idxs = np.argsort(echo_list)
     echo_list = echo_list[sort_idxs]
-    image = image[:, :, :, sort_idxs]
+    image = image[..., sort_idxs]
 
     return image, data.affine, echo_list
+
+
+def b0_ge():
+    """Fetches b0/ge dataset
+
+    Returns
+    -------
+    numpy.ndarray
+        image data - Magnitude
+    numpy.ndarray
+        image data - Phase
+    numpy.ndarray
+        affine matrix for image data
+    numpy.ndarray
+        array of echo times, in seconds
+
+    """
+
+    # Initialise hard-coded list of file names that are the expected files
+    # in this test dataset. If the actual files in the directory don't match
+    # this list this means that the test dataset has been corrupted.
+    # Note that these file names are sorted alphabetically and not sorted by
+    # increasing echo time. The sort by echo time will be done later below.
+    expected_filenames = ['00009__3D_B0_map_VOL_e1.json',
+                          '00009__3D_B0_map_VOL_e1.nii.gz',
+                          '00009__3D_B0_map_VOL_e2.json',
+                          '00009__3D_B0_map_VOL_e2.nii.gz']
+
+    # Initialise path to b0/ge
+    dir_b0_ge = os.path.join(DIR_DATA, "b0", "ge")
+
+    # Get filepaths in directory and check their names match expected_filenames
+    filepaths = get_filepaths(dir_b0_ge, expected_filenames)
+
+    # Load magnitude, real and imaginary data and corresponding echo times
+    magnitude = []
+    real = []
+    imaginary = []
+    echo_list = []
+    for filepath in filepaths:
+
+        if filepath.endswith(".nii.gz"):
+
+            # Load NIfTI and save the magnitude data (index 0)
+            data = nib.load(filepath)
+            magnitude.append(data.get_fdata()[..., 0])
+            # Save the real data (index 1) - I STILL NEED TO CONFIRM!
+            real.append(data.get_fdata()[..., 1])
+            # Save the imaginary data (index 2) - I STILL NEED TO CONFIRM!
+            imaginary.append(data.get_fdata()[..., 2])
+
+        elif filepath.endswith(".json"):
+
+            # Retrieve list of echo times in the original order
+            with open(filepath, 'r') as json_file:
+                hdr = json.load(json_file)
+            echo_list.append(hdr['EchoTime'])
+
+    # Move echo dimension to 4th dimension
+    magnitude = np.moveaxis(np.array(magnitude), 0, -1)
+    real = np.moveaxis(np.array(real), 0, -1)
+    imaginary = np.moveaxis(np.array(imaginary), 0, -1)
+    phase = np.arctan2(imaginary, real)
+
+    echo_list = np.array(echo_list)
+
+    # Sort by increasing echo time
+    sort_idxs = np.argsort(echo_list)
+    echo_list = echo_list[sort_idxs]
+    magnitude = magnitude[..., sort_idxs]
+    phase = phase[..., sort_idxs]
+
+    return magnitude, phase, data.affine, echo_list
+
+
+def b0_siemens_1():
+    """Fetches b0/siemens_1 dataset
+
+    Returns
+    -------
+    numpy.ndarray
+        image data - Magnitude
+    numpy.ndarray
+        image data - Phase
+    numpy.ndarray
+        affine matrix for image data
+    numpy.ndarray
+        array of echo times, in seconds
+
+    """
+
+    # Initialise hard-coded list of file names that are the expected files
+    # in this test dataset. If the actual files in the directory don't match
+    # this list this means that the test dataset has been corrupted.
+    # Note that these file names are sorted alphabetically and not sorted by
+    # increasing echo time. The sort by echo time will be done later below.
+    expected_filenames = ['00010__bh_b0map_3D_default_e1.json',
+                          '00010__bh_b0map_3D_default_e1.nii.gz',
+                          '00010__bh_b0map_3D_default_e2.json',
+                          '00010__bh_b0map_3D_default_e2.nii.gz',
+                          '00011__bh_b0map_3D_default_e1.json',
+                          '00011__bh_b0map_3D_default_e1.nii.gz',
+                          '00011__bh_b0map_3D_default_e2.json',
+                          '00011__bh_b0map_3D_default_e2.nii.gz']
+
+    # Initialise path to b0/siemens_1
+    dir_b0_siemens_1 = os.path.join(DIR_DATA, "b0", "siemens_1")
+
+    # Get filepaths in directory and check their names match expected_filenames
+    filepaths = get_filepaths(dir_b0_siemens_1, expected_filenames)
+
+    # Load magnitude, real and imaginary data and corresponding echo times
+    magnitude = []
+    phase = []
+    echo_list = []
+    for filepath in filepaths:
+
+        if filepath.endswith(".nii.gz"):
+
+            # Load NIfTI
+            data = nib.load(filepath)
+            if "00010" in filepath:
+                # Save the magnitude data
+                magnitude.append(data.get_fdata())
+            elif "00011" in filepath:
+                # Save the phase data
+                phase.append(data.get_fdata())
+
+        elif filepath.endswith(".json"):
+
+            # Retrieve list of echo times in the original order
+            with open(filepath, 'r') as json_file:
+                hdr = json.load(json_file)
+            echo_list.append(hdr['EchoTime'])
+
+    # Move echo dimension to 4th dimension
+    magnitude = np.swapaxes(np.array(magnitude), 0, -1)
+    phase = np.swapaxes(np.array(phase), 0, -1)
+    echo_list = np.unique(np.array(echo_list))
+
+    # Sort by increasing echo time
+    sort_idxs = np.argsort(echo_list)
+    echo_list = echo_list[sort_idxs]
+    magnitude = magnitude[..., sort_idxs]
+    phase = phase[..., sort_idxs]
+
+    return magnitude, phase, data.affine, echo_list
+
+
+def b0_siemens_2():
+    """Fetches b0/siemens_2 dataset
+
+    Returns
+    -------
+    numpy.ndarray
+        image data - Magnitude
+    numpy.ndarray
+        image data - Phase
+    numpy.ndarray
+        affine matrix for image data
+    numpy.ndarray
+        array of echo times, in seconds
+
+    """
+
+    # Initialise hard-coded list of file names that are the expected files
+    # in this test dataset. If the actual files in the directory don't match
+    # this list this means that the test dataset has been corrupted.
+    # Note that these file names are sorted alphabetically and not sorted by
+    # increasing echo time. The sort by echo time will be done later below.
+    expected_filenames = ['00044__bh_b0map_fa3_default_e1.json',
+                          '00044__bh_b0map_fa3_default_e1.nii.gz',
+                          '00044__bh_b0map_fa3_default_e2.json',
+                          '00044__bh_b0map_fa3_default_e2.nii.gz',
+                          '00045__bh_b0map_fa3_default_e1.json',
+                          '00045__bh_b0map_fa3_default_e1.nii.gz',
+                          '00045__bh_b0map_fa3_default_e2.json',
+                          '00045__bh_b0map_fa3_default_e2.nii.gz']
+
+    # Initialise path to b0/siemens_2
+    dir_b0_siemens_2 = os.path.join(DIR_DATA, "b0", "siemens_2")
+
+    # Get filepaths in directory and check their names match expected_filenames
+    filepaths = get_filepaths(dir_b0_siemens_2, expected_filenames)
+
+    # Load magnitude, real and imaginary data and corresponding echo times
+    magnitude = []
+    phase = []
+    echo_list = []
+    for filepath in filepaths:
+
+        if filepath.endswith(".nii.gz"):
+
+            # Load NIfTI
+            data = nib.load(filepath)
+            if "00044" in filepath:
+                # Save the magnitude data
+                magnitude.append(data.get_fdata())
+            elif "00045" in filepath:
+                # Save the phase data
+                phase.append(data.get_fdata())
+
+        elif filepath.endswith(".json"):
+
+            # Retrieve list of echo times in the original order
+            with open(filepath, 'r') as json_file:
+                hdr = json.load(json_file)
+            echo_list.append(hdr['EchoTime'])
+
+    # Move echo dimension to 4th dimension
+    magnitude = np.moveaxis(np.array(magnitude), 0, -1)
+    phase = np.moveaxis(np.array(phase), 0, -1)
+    echo_list = np.unique(np.array(echo_list))
+
+    # Sort by increasing echo time
+    sort_idxs = np.argsort(echo_list)
+    echo_list = echo_list[sort_idxs]
+    magnitude = magnitude[..., sort_idxs]
+    phase = phase[..., sort_idxs]
+
+    return magnitude, phase, data.affine, echo_list
