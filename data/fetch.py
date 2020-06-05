449693c4e3051605f42eb7439108907ba377f7e6
Charlotte Buchanan <Charlotte.Buchanan@nottingham.ac.uk>
2020-06-05 09:29:03 +0100

    modified fetch.py to include philips R2*

--- data/fetch.py
+++ data/fetch.py
@@ -172,3 +172,82 @@ def r2star_ge():
     image = image[:, :, :, sort_idxs]
 
     return image, data.affine, echo_list
+
+def r2star_philips():
+    """Fetches r2star/philips dataset
+
+    Returns
+    -------
+    numpy.ndarray
+        image data
+    numpy.ndarray
+        affine matrix for image data
+    numpy.ndarray
+        array of echo times, in seconds
+
+    """
+
+    # Initialise hard-coded list of file names that are the expected files
+    # in this test dataset. If the actual files in the directory don't match
+    # this list this means that the test dataset has been corrupted.
+    # Note that these file names are sorted alphabetically and not sorted by
+    # increasing echo time. The sort by echo time will be done later below.
+    expected_filenames = ['01401__Kidney_T2star_m-FFE_3x3x5_SPIR_volume_inphase_e1.json',
+                          '01401__Kidney_T2star_m-FFE_3x3x5_SPIR_volume_inphase_e1.nii.gz',
+                          '01401__Kidney_T2star_m - FFE_3x3x5_SPIR_volume_inphase_e10.json',
+                          '01401__Kidney_T2star_m-FFE_3x3x5_SPIR_volume_inphase_e10.nii.gz',
+                          '01401__Kidney_T2star_m - FFE_3x3x5_SPIR_volume_inphase_e11.json',
+                          '01401__Kidney_T2star_m-FFE_3x3x5_SPIR_volume_inphase_e11.nii.gz',
+                          '01401__Kidney_T2star_m - FFE_3x3x5_SPIR_volume_inphase_e12.json',
+                          '01401__Kidney_T2star_m-FFE_3x3x5_SPIR_volume_inphase_e12.nii.gz',
+                          '01401__Kidney_T2star_m - FFE_3x3x5_SPIR_volume_inphase_e2.json',
+                          '01401__Kidney_T2star_m-FFE_3x3x5_SPIR_volume_inphase_e2.nii.gz',
+                          '01401__Kidney_T2star_m - FFE_3x3x5_SPIR_volume_inphase_e3.json',
+                          '01401__Kidney_T2star_m-FFE_3x3x5_SPIR_volume_inphase_e3.nii.gz',
+                          '01401__Kidney_T2star_m - FFE_3x3x5_SPIR_volume_inphase_e4.json',
+                          '01401__Kidney_T2star_m-FFE_3x3x5_SPIR_volume_inphase_e4.nii.gz',
+                          '01401__Kidney_T2star_m - FFE_3x3x5_SPIR_volume_inphase_e5.json',
+                          '01401__Kidney_T2star_m-FFE_3x3x5_SPIR_volume_inphase_e5.nii.gz',
+                          '01401__Kidney_T2star_m - FFE_3x3x5_SPIR_volume_inphase_e6.json',
+                          '01401__Kidney_T2star_m-FFE_3x3x5_SPIR_volume_inphase_e6.nii.gz',
+                          '01401__Kidney_T2star_m - FFE_3x3x5_SPIR_volume_inphase_e7.json',
+                          '01401__Kidney_T2star_m-FFE_3x3x5_SPIR_volume_inphase_e7.nii.gz',
+                          '01401__Kidney_T2star_m - FFE_3x3x5_SPIR_volume_inphase_e8.json',
+                          '01401__Kidney_T2star_m-FFE_3x3x5_SPIR_volume_inphase_e8.nii.gz',
+                          '01401__Kidney_T2star_m - FFE_3x3x5_SPIR_volume_inphase_e9.json',
+                          '01401__Kidney_T2star_m-FFE_3x3x5_SPIR_volume_inphase_e9.nii.gz']
+
+    # Initialise path to r2star/philips
+    dir_r2star_philips = os.path.join(DIR_DATA, "r2star", "philips")
+
+    # Get filepaths in directory and check their names match expected_filenames
+    filepaths = get_filepaths(dir_r2star_philips, expected_filenames)
+
+    # Load magnitude data and corresponding echo times (in the orig)
+    image = []
+    echo_list = []
+    for filepath in filepaths:
+
+        if filepath.endswith(".nii.gz"):
+
+            # Load NIfTI and only save the magnitude data (index 0)
+            data = nib.load(filepath)
+            image.append(data.get_fdata()[..., 0])
+
+        elif filepath.endswith(".json"):
+
+            # Retrieve list of echo times in the original order
+            with open(filepath, 'r') as json_file:
+                hdr = json.load(json_file)
+            echo_list.append(hdr['EchoTime'])
+
+    # Move echo dimension to 4th dimension
+    image = np.moveaxis(np.array(image), 0, -1)
+    echo_list = np.array(echo_list)
+
+    # Sort by increasing echo time
+    sort_idxs = np.argsort(echo_list)
+    echo_list = echo_list[sort_idxs]
+    image = image[:, :, :, sort_idxs]
+
+    return image, data.affine, echo_list
