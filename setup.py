6c26d49ca17b27e08458379e1126eccdbc1ed519
Fabio Nery <fabiornery@gmail.com>
2020-07-05 13:17:09 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ setup(
     install_requires=requirements,
 
     package_data={
-        # If any package contains *.txt or *.rst files, include them:
+        # If any package contains any files with the extensions below, include them:
         "": ["*.json", "*.nii.gz", "*.bval", "*.bval_before_manual_correction", "*.bvec", "*.png"],
     },
 )
