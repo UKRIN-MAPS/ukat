389456e983564964650c9de4c55716ab22ab0631
fnery <fabiornery@gmail.com>
2020-07-05 13:11:54 +0100

    MNT: improved installation process/documentation

--- setup.py
+++ setup.py
@@ -1,18 +1,23 @@
-from distutils.core import setup
+from setuptools import setup, find_packages
+
+# Get requirements from text file
+with open('requirements.txt') as f:
+    requirements = f.read().splitlines()
 
 setup(
-    name='ukat',
-    version='0.1.0',
-    description='UKRIN Kidney Analysis Toolbox',
-    url='https://github.com/UKRIN-MAPS/ukat',
-    license='GPL-3.0',
-    packages=['ukat', ],
-    install_requires=[
-        'numpy>=1.18.1',
-        'nibabel>=3.0.2',
-        'matplotlib>=3.1.3',
-        'scikit-image>=0.16.2',
-        'scipy>=1.4.1',
-        'dipy>=1.0.0',
-    ],
+    name="ukat",
+    version="0.1.0",
+    description="UKRIN Kidney Analysis Toolbox",
+    url="https://github.com/UKRIN-MAPS/ukat",
+    license="GPL-3.0",
+
+    packages=find_packages(),
+    # packages=find_packages(exclude=("tests*")), # Want to exclude tests?
+
+    install_requires=requirements,
+
+    package_data={
+        # If any package contains *.txt or *.rst files, include them:
+        "": ["*.json", "*.nii.gz", "*.bval", "*.bval_before_manual_correction", "*.bvec", "*.png"],
+    },
 )
