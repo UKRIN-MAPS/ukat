9602b9dcfdc1664e038e8734957297d077f37f40
Alex Daniel <alexander.daniel@nottingham.ac.uk>
2020-07-30 11:52:29 +0100

    Added tests for magnitude correction of different dimensional data

--- ukat/mapping/tests/test_t1.py
+++ ukat/mapping/tests/test_t1.py
@@ -1,9 +1,8 @@
 import numpy as np
+import numpy.testing as npt
 import pytest
 from ukat.mapping.t1 import magnitude_correct
 import ukat.utils.tools as tools
-
-np.random.seed(1)
 
 # Magnitude Correction Testing
 
@@ -15,6 +14,10 @@ comp = real + imag * (0 + 1j)
 # "Simple data" uses the last dimension to hold real and imaginary
 # components respectively
 simple = np.array([real, imag]).T
+
+correct_array = np.array([-67432.70678981, -55331.41094351, -20507.34797579,
+                          -14790.92130329, 10683.72318061])
+
 
 # TODO e2e test with scanner data
 
@@ -33,3 +36,46 @@ def test_complex_conversion_shape():
     # two i.e. isn't real and imag
     with pytest.raises(ValueError):
         corrected = magnitude_correct(simple[:, 0])
+
+
+def test_input_dimensions():
+    
+    # Tile existing data to increase dimensions
+    # Comp tested up to 4D i.e. [x, y, z, TI]
+    comp_2d = np.tile(comp, (4, 1))
+    comp_3d = np.tile(comp, (4, 4, 1))
+    comp_4d = np.tile(comp, (4, 4, 4, 1))
+    # Simple tested up to 5D i.e. [x, y, z, TI, re/im]
+    simp_3d = np.tile(simple, (4, 1, 1))
+    simp_4d = np.tile(simple, (4, 4, 1, 1))
+    simp_5d = np.tile(simple, (4, 4, 4, 1, 1))
+    
+    corrected = magnitude_correct(comp_2d)
+    assert corrected.shape == (4, 5)
+    npt.assert_allclose(corrected, np.tile(correct_array, (4, 1)),
+                        rtol=1e-9, atol=1e-9)
+
+    corrected = magnitude_correct(comp_3d)
+    assert corrected.shape == (4, 4, 5)
+    npt.assert_allclose(corrected, np.tile(correct_array, (4, 4, 1)),
+                        rtol=1e-9, atol=1e-9)
+
+    corrected = magnitude_correct(comp_4d)
+    assert corrected.shape == (4, 4, 4, 5)
+    npt.assert_allclose(corrected, np.tile(correct_array, (4, 4, 4, 1)),
+                        rtol=1e-9, atol=1e-9)
+
+    corrected = magnitude_correct(simp_3d)
+    assert corrected.shape == (4, 5)
+    npt.assert_allclose(corrected, np.tile(correct_array, (4, 1)),
+                        rtol=1e-9, atol=1e-9)
+
+    corrected = magnitude_correct(simp_4d)
+    assert corrected.shape == (4, 4, 5)
+    npt.assert_allclose(corrected, np.tile(correct_array, (4, 4, 1)),
+                        rtol=1e-9, atol=1e-9)
+
+    corrected = magnitude_correct(simp_5d)
+    assert corrected.shape == (4, 4, 4, 5)
+    npt.assert_allclose(corrected, np.tile(correct_array, (4, 4, 4, 1)),
+                        rtol=1e-9, atol=1e-9)
