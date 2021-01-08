c6d75705c696ee00240d6d28a938da32ac77c8e6
JSousa-UoL <j.g.sousa@sheffield.ac.uk>
2021-01-08 17:12:05 +0000

    Github issues Add classfiers to setup.py #56 and Simplify non-editable installation #73 resolved

--- setup.py
+++ setup.py
@@ -10,15 +10,15 @@ with open('README.md', encoding='utf-8') as f:
 
 setup(
     name="ukat",
-    version="0.1.0",
+    version="0.1.0", # Soon to be made public?
     description="UKRIN Kidney Analysis Toolbox",
     long_description = long_description,
     url="https://github.com/UKRIN-MAPS/ukat",
-    license="GPL-3.0",
+    license="GPL-3.0", # Check if it should be Apache 2.0.
 
+    python_requires='>=3.5, <4',
     packages=find_packages(),
     # packages=find_packages(exclude=("tests*")), # Want to exclude tests?
-
     install_requires=requirements,
 
     package_data={
@@ -30,4 +30,33 @@ setup(
              "*.bvec",
              "*.png"],
     },
+
+    # Classifiers - the purpose in the future is to create a wheel (pip install wheel) and then upload to PYPI
+    classifiers=[
+        # How mature is this project? Common values are
+        #   3 - Alpha
+        #   4 - Beta
+        #   5 - Production/Stable
+        'Development Status :: 3 - Alpha',
+
+        # Indicate who your project is intended for
+        'Intended Audience :: Developers',
+        'Intended Audience :: Science/Research',
+        'Topic :: Scientific/Engineering',
+        'Environment :: Console',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3',
+        # Specify the Python versions you support here. In particular, ensure
+        # that you indicate you support Python 3. These classifiers are *not*
+        # checked by 'pip install'. See instead 'python_requires' below.
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+
+        # Pick your license as you wish
+        'License :: OSI Approved :: GPL-3.0 License',
+        #'License :: OSI Approved :: Apache Software License',
+
+
+    ],
 )
