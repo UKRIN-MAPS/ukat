445fd1a27ec48cd680ff35b91deb2b6831b4a723
Alex Daniel <alexander.daniel@nottingham.ac.uk>
2021-05-05 15:13:45 +0100

    Added ukrin_b option
    Uses only b-vals from consensus paper

--- ukat/mapping/diffusion.py
+++ ukat/mapping/diffusion.py
@@ -88,39 +88,31 @@ class ADC:
     Attributes
     ----------
     adc : np.ndarray
-        The estimated ADC in mm^2/s
+        The estimated ADC in mm^2/s.
     adc_err : np.ndarray
-        The certainty in the fit of `adc` in mm^2/s
+        The certainty in the fit of `adc` in mm^2/s.
     shape : tuple
-        The shape of the ADC map
+        The shape of the ADC map.
     n_vox : int
-        The number of voxels in the map
+        The number of voxels in the map.
     bvals : 1d numpy array
-        All b-values that will be used to generate the maps in s/mm^2
+        All b-values that will be used to generate the maps in s/mm^2.
     u_bvals : 1d numpy array
         The unique b-values used in the experiment e.g. if the experiment
         acquires a single b-0 volume and 64 volumes with b=600 s/mm^2 in
-        different directions, u_bvals will be [0, 600]
+        different directions, u_bvals will be [0, 600].
     n_bvals : int
-        The number of unique b-values acquired in the experiment
-    bvecs : (N, 3) numpy array
-        All b-vectors that will be used to generate the maps
-    u_bvecs : (M, 3) numpy array
-        The unique b-vectors used in the experiment e.g. if the experiment
-        acquires six directions at 10 gradient strengths, u_bvecs will be a
-        6 x 3 numpy array
-    n_bvecs : int
-        The number of unique b-vectors acquired in the experiment
+        The number of unique b-values acquired in the experiment.
     n_grad : 1d numpy array
         Total number of diffusion values/vectors acquired e.g. if the
         experiment acquires six directions at 10 gradient strengths and a
-        b-0 volume, n_grad will be 61
+        b-0 volume, n_grad will be 61.
     pixel_array_mean : np.ndarray
         The average of the `pixel_array`across bvecs e.g. if `pixel_array`
         contains six volumes acquired with b=600 s/mm^2 in different
         directions, these six volumes will be averaged together.
     """
-    def __init__(self, pixel_array, affine, bvals, bvecs=None, mask=None):
+    def __init__(self, pixel_array, affine, bvals, mask=None, ukrin_b=False):
         """Initialise a ADC class instance.
 
         Parameters
@@ -128,44 +120,43 @@ class ADC:
         pixel_array : (..., N) np.ndarray
             A array containing the signal from each voxel at each
             diffusion sensitising parameter. The final dimension should be
-            different diffusion weightings/directions
+            different diffusion weightings/directions.
         affine : np.ndarray
             A matrix giving the relationship between voxel coordinates and
             world coordinates.
         bvals : (N,) np.array
             An array of the b-values used for the last dimension of the raw
             data. In s/mm^2.
-        bvecs : (N, 3) np.array, optional
-            An array of the b-vectors used for the last dimension of the raw
-            data. In s/mm^2.
         mask : np.ndarray, optional
             A boolean mask of the voxels to fit. Should be the shape of the
             desired map rather than the raw data i.e. omit the last dimension.
+        ukrin_b : bool, optional
+            If True, only b-values of 0, 100, 200 and 800 s/mm^2 will be
+            included in the ADC fit. This aligns with Ljimani A, et al.
+            Consensus-based technical recommendations for clinical translation
+            of renal diffusion-weighted MRI.
+            Magn Reson Mater Phy 2020;33:177–195
+            doi: 10.1007/s10334-019-00790-y.
+            If False, all b-values supplied will be used to fit ADC.
         """
+        ukrin_b_test = np.array([0, 100, 200, 800])
         # Sanity checks
         assert (pixel_array.shape[-1]
                 == len(bvals)), 'Number of bvals does not match number of ' \
                                 'gradients in pixel_array'
-        if bvecs is not None:
-            if bvecs.shape[1] != 3 and bvecs.shape[0] == 3:
-                bvecs = bvecs.T
-                warnings.warn(f'bvecs should be (N, 3). Because your bvecs '
-                              'array is {bvecs.shape} it has been '
-                              'transposed to {bvecs.T.shape}.')
-            assert (bvecs.shape[1] == 3)
-            assert (pixel_array.shape[-1]
-                    == bvecs.shape[0]), 'Number of bvecs does not match ' \
-                                        'number of gradients in pixel_array'
-        self.pixel_array = pixel_array
+
+        if ukrin_b:
+            self.b_mask = np.isin(bvals, ukrin_b_test)
+        else:
+            self.b_mask = np.full(len(bvals), True, dtype=bool)
+
+        self.pixel_array = pixel_array[..., self.b_mask]
         self.shape = pixel_array.shape[:-1]
         self.n_vox = np.prod(self.shape)
-        self.bvals = bvals
-        self.bvecs = bvecs
+        self.bvals = bvals[self.b_mask]
         self.n_grad = len(self.bvals)
         self.u_bvals = unique_bvals_tolerance(self.bvals, 1)
         self.n_bvals = len(self.u_bvals)
-        self.u_bvecs = np.unique(self.bvecs)
-        self.n_bvecs = len(self.u_bvecs)
         self.affine = affine
         # Generate a mask if there isn't one specified
         if mask is None:
@@ -209,8 +200,8 @@ class ADC:
         with tqdm(total=idx.size) as progress:
             for ind in idx:
                 sig = signal[ind, :]
-                adc_map[ind], adc_err[ind] = self.__fit_signal__(sig,
-                                                                 self.u_bvals)
+                adc_map[ind], adc_err[ind] = \
+                    self.__fit_signal__(sig, self.u_bvals)
                 progress.update(1)
         adc_map[adc_map < 0] = 0
         adc_err[adc_map < 0] = 0
@@ -277,41 +268,42 @@ class DTI:
     Attributes
     ----------
     md : np.ndarray
-        The estimated mean diffusivity values in mm^2/s
+        The estimated mean diffusivity values in mm^2/s.
     fa : np.ndarray
-        The estimated fractional anisotropy values
+        The estimated fractional anisotropy values.
     color_fa : np.ndarray
         The estimated directional fractional anisotropy represented as red,
         green and blue corresponding to correspond to fractional anisotropy
-        in the x, y and z directions respectively
+        in the x, y and z directions respectively.
     shape : tuple
         The shape of the resulting maps
     bvals : 1d numpy array
-        All b-values that will be used to generate the maps in s/mm^2
+        All b-values that will be used to generate the maps in s/mm^2.
     u_bvals : 1d numpy array
         The unique b-values used in the experiment e.g. if the experiment
         acquires a single b-0 volume and 64 volumes with b=600 s/mm^2 in
-        different directions, u_bvals will be [0, 600]
+        different directions, u_bvals will be [0, 600].
     n_bvals : int
-        The number of unique b-values acquired in the experiment
+        The number of unique b-values acquired in the experiment.
     bvecs : (N, 3) numpy array
-        All b-vectors that will be used to generate the maps
+        All b-vectors that will be used to generate the maps.
     u_bvecs : (M, 3) numpy array
         The unique b-vectors used in the experiment e.g. if the experiment
         acquires six directions at 10 gradient strengths, u_bvecs will be a
-        6 x 3 numpy array
+        6 x 3 numpy array.
     n_bvecs : int
-        The number of unique b-vectors acquired in the experiment
+        The number of unique b-vectors acquired in the experiment.
     n_grad : 1d numpy array
         Total number of diffusion values/vectors acquired e.g. if the
         experiment acquires six directions at 10 gradient strengths and a
-        b-0 volume, n_grad will be 61
+        b-0 volume, n_grad will be 61.
     gtab : dipy GradientTable
-        The dipy gradient table used to generate maps
+        The dipy gradient table used to generate maps.
     tensor_fit : dipy TensorModel after fitting
-        The fit dipy tensor model, can be used to recall additional parameters
+        The fit dipy tensor model, can be used to recall additional parameters.
     """
-    def __init__(self, pixel_array, affine, bvals, bvecs, mask=None):
+    def __init__(self, pixel_array, affine, bvals, bvecs, mask=None,
+                 ukrin_b=False):
         """Initialise a DTI class instance.
 
         Parameters
@@ -319,7 +311,7 @@ class DTI:
         pixel_array : (..., N) np.ndarray
             A array containing the signal from each voxel at each
             diffusion sensitising parameter. The final dimension should be
-            different diffusion weightings/directions
+            different diffusion weightings/directions.
         affine : np.ndarray
             A matrix giving the relationship between voxel coordinates and
             world coordinates.
@@ -332,7 +324,16 @@ class DTI:
         mask : np.ndarray, optional
             A boolean mask of the voxels to fit. Should be the shape of the
             desired map rather than the raw data i.e. omit the last dimension.
+        ukrin_b : bool, optional
+            If True, only b-values of 0, 100, 200 and 800 s/mm^2 will be
+            included in the fit. This aligns with Ljimani A, et al.
+            Consensus-based technical recommendations for clinical translation
+            of renal diffusion-weighted MRI.
+            Magn Reson Mater Phy 2020;33:177–195
+            doi: 10.1007/s10334-019-00790-y.
+            If False, all b-values supplied will be used.
         """
+        ukrin_b_test = np.array([0, 100, 200, 800])
         # Some sanity checks
         assert (pixel_array.shape[-1]
                 == len(bvals)), 'Number of bvals does not match number of ' \
@@ -348,20 +349,25 @@ class DTI:
                                                           'number of ' \
                                                           'gradients in ' \
                                                           'pixel_array'
-        self.pixel_array = pixel_array
-        self.shape = pixel_array.shape[:-1]
-        self.bvals = bvals
-        self.bvecs = bvecs
+        if ukrin_b:
+            self.b_mask = np.isin(bvals, ukrin_b_test)
+        else:
+            self.b_mask = np.full(len(bvals), True, dtype=bool)
+
+        self.pixel_array = pixel_array[..., self.b_mask]
+        self.shape = self.pixel_array.shape[:-1]
+        self.bvals = bvals[self.b_mask]
+        self.bvecs = bvecs[self.b_mask, :]
         self.n_grad = len(self.bvals)
         self.u_bvals = unique_bvals_tolerance(self.bvals, 1)
         self.n_bvals = len(self.u_bvals)
-        self.u_bvecs = np.unique(self.bvecs)
+        self.u_bvecs = np.unique(self.bvecs, axis=0)
         self.n_bvecs = len(self.u_bvecs)
         self.affine = affine
         self.mask = mask
-        self.gtab = gradient_table(bvals, bvecs, b0_threshold=0)
+        self.gtab = gradient_table(self.bvals, self.bvecs, b0_threshold=0)
         tensor_model = TensorModel(self.gtab)
-        self.tensor_fit = tensor_model.fit(pixel_array, mask=self.mask)
+        self.tensor_fit = tensor_model.fit(self.pixel_array, mask=self.mask)
         self.md = self.tensor_fit.md
         self.fa = self.tensor_fit.fa
         self.color_fa = self.tensor_fit.color_fa
--- ukat/mapping/tests/test_diffusion.py
+++ ukat/mapping/tests/test_diffusion.py
@@ -81,23 +81,11 @@ class TestADC:
 
         with pytest.raises(AssertionError):
             mapper = ADC(self.pixel_array, self.affine, self.bvals[:-2],
-                         self.bvecs, self.mask)
-
-    def test_missmatched_raw_data_and_bvecs(self):
-
-        with pytest.raises(AssertionError):
-            mapper = ADC(self.pixel_array, self.affine, self.bvals,
-                         self.bvecs[:-1, :], self.mask)
-
-    def test_bvecs_transpose(self):
-
-        with pytest.warns(UserWarning):
-            mapper = ADC(self.pixel_array, self.affine, self.bvals,
-                         self.bvecs.T, self.mask)
+                         self.mask)
 
     def test_fail_to_fit(self):
         mapper = ADC(self.pixel_array[..., ::-1], self.affine, self.bvals,
-                     self.bvecs.T, self.mask)
+                     self.mask)
         assert np.abs(mapper.adc.mean()) < 1e-6
 
     def test_real_data(self):
@@ -105,8 +93,7 @@ class TestADC:
         gold_standard_adc = [0.00146, 0.001057, 0.0, 0.005391]
         gold_standard_adc_err = [0.000128, 0.000143, 0.0, 0.001044]
         # Test maps
-        mapper = ADC(self.pixel_array, self.affine, self.bvals, self.bvecs,
-                     self.mask)
+        mapper = ADC(self.pixel_array, self.affine, self.bvals, self.mask)
         adc_stats = arraystats.ArrayStats(mapper.adc).calculate()
         adc_err_stats = arraystats.ArrayStats(mapper.adc_err).calculate()
         npt.assert_allclose([adc_stats['mean']['3D'], adc_stats['std']['3D'],
@@ -118,36 +105,28 @@ class TestADC:
                              adc_err_stats['max']['3D']],
                             gold_standard_adc_err, rtol=5e-3, atol=1e-7)
 
-    def test_no_bvecs(self):
+    def test_ukrin_b(self):
         # Gold standard statistics
-        gold_standard_adc = [0.00146, 0.001057, 0.0, 0.005391]
-        gold_standard_adc_err = [0.000128, 0.000143, 0.0, 0.001044]
-        # Calculate mean across bvecs before mapping to simulate scanner
-        # averaging of ADC data
-        u_bvals = np.unique(self.bvals)
-        pixel_array_mean = np.zeros((*self.pixel_array.shape[:3],
-                                     len(u_bvals)))
-        for ind, bval in enumerate(u_bvals):
-            pixel_array_mean[..., ind] \
-                = np.mean(self.pixel_array[..., self.bvals == bval], axis=-1)
-
-        # Calculate map
-        mapper = ADC(pixel_array_mean, self.affine, u_bvals, bvecs=None,
-                     mask=self.mask)
+        gold_standard_adc = [0.001401, 0.000995, 0.0, 0.004789]
+        gold_standard_adc_err = [0.000239, 0.000317, 0.0, 0.00275]
+        # Test maps
+        mapper = ADC(self.pixel_array, self.affine, self.bvals, self.mask,
+                     ukrin_b=True)
+        assert mapper.n_bvals == 4
+        npt.assert_array_equal(mapper.u_bvals, np.array([0, 100, 200, 800]))
         adc_stats = arraystats.ArrayStats(mapper.adc).calculate()
         adc_err_stats = arraystats.ArrayStats(mapper.adc_err).calculate()
         npt.assert_allclose([adc_stats['mean']['3D'], adc_stats['std']['3D'],
                              adc_stats['min']['3D'], adc_stats['max']['3D']],
-                            gold_standard_adc, rtol=1e-4, atol=1e-7)
+                            gold_standard_adc, rtol=5e-4, atol=5e-7)
         npt.assert_allclose([adc_err_stats['mean']['3D'],
                              adc_err_stats['std']['3D'],
                              adc_err_stats['min']['3D'],
                              adc_err_stats['max']['3D']],
-                            gold_standard_adc_err, rtol=5e-3, atol=1e-7)
+                            gold_standard_adc_err, rtol=1e-3, atol=5e-7)
 
     def test_to_nifti(self):
-        mapper = ADC(self.pixel_array, self.affine, self.bvals, self.bvecs,
-                     self.mask)
+        mapper = ADC(self.pixel_array, self.affine, self.bvals, self.mask)
 
         os.makedirs('test_output', exist_ok=True)
 
@@ -221,6 +200,32 @@ class TestDTI:
         # Test maps
         mapper = DTI(self.pixel_array, self.affine, self.bvals, self.bvecs,
                      self.mask)
+        md_stats = arraystats.ArrayStats(mapper.md).calculate()
+        fa_stats = arraystats.ArrayStats(mapper.fa).calculate()
+        color_fa_stats = arraystats.ArrayStats(mapper.color_fa).calculate()
+        npt.assert_allclose([md_stats['mean']['3D'], md_stats['std']['3D'],
+                             md_stats['min']['3D'], md_stats['max']['3D']],
+                            gold_standard_md, rtol=1e-6, atol=1e-4)
+        npt.assert_allclose([fa_stats['mean']['3D'], fa_stats['std']['3D'],
+                             fa_stats['min']['3D'], fa_stats['max']['3D']],
+                            gold_standard_fa, rtol=1e-6, atol=1e-4)
+        npt.assert_allclose([color_fa_stats['mean']['4D'],
+                             color_fa_stats['std']['4D'],
+                             color_fa_stats['min']['4D'],
+                             color_fa_stats['max']['4D']],
+                            gold_standard_color_fa, rtol=1e-6, atol=1e-4)
+
+    def test_ukrin_b(self):
+        # Gold standard statistics
+        gold_standard_md = [0.001611, 0.001437, 0.0, 0.02951]
+        gold_standard_fa = [0.446954, 0.291819, 0.0, 1.0]
+        gold_standard_color_fa = [0.216434, 0.219389, 0.0, 0.99972]
+
+        # Test maps
+        mapper = DTI(self.pixel_array, self.affine, self.bvals, self.bvecs,
+                     self.mask, ukrin_b=True)
+        assert mapper.n_bvals == 4
+        npt.assert_array_equal(mapper.u_bvals, np.array([0, 100, 200, 800]))
         md_stats = arraystats.ArrayStats(mapper.md).calculate()
         fa_stats = arraystats.ArrayStats(mapper.fa).calculate()
         color_fa_stats = arraystats.ArrayStats(mapper.color_fa).calculate()
