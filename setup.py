c45d8851f3dc82a5009f0b427fa75c024172dc5b
Alex Daniel <alexander.daniel@nottingham.ac.uk>
2021-05-25 14:11:01 +0100

    Bump to rc.2

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open('README.md', encoding='utf-8') as f:
 
 setup(
     name="ukat",
-    version="0.3.0-rc.1",
+    version="0.3.0-rc.2",
     description="UKRIN Kidney Analysis Toolbox",
     long_description=long_description,
     long_description_content_type="text/markdown",
