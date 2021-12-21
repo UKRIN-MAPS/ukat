78bf526b053ec9d2b1e6a89602fadd85205ee317
Alex Daniel <alexander.daniel@nottingham.ac.uk>
2021-12-21 17:27:02 +0000

    Added rest of tests

--- ukat/segment/tests/test_whole_kidney.py
+++ ukat/segment/tests/test_whole_kidney.py
@@ -1,6 +1,8 @@
+import csv
 import os
-import numpy.testing as npt
 import shutil
+import numpy.testing as npt
+import pytest
 
 from ukat.data import fetch
 from ukat.segment.whole_kidney import Segmentation
@@ -61,3 +63,52 @@ class TestSegmentation:
     def test_get_rkv(self):
         expected = 121.80702174282074
         assert self.segmentation.get_rkv() == expected
+
+    def test_save_volumes_csv(self):
+        expected = [['total', 'left', 'right'],
+                    ['240.00054654884337',
+                     '118.19352480602264',
+                     '121.80702174282074']]
+
+        os.makedirs('test_output', exist_ok=True)
+        self.segmentation.save_volumes_csv('test_output/volumes.csv')
+        output_files = os.listdir('test_output')
+
+        assert 'volumes.csv' in output_files
+
+        with open('test_output/volumes.csv', 'r') as csv_file:
+            reader = csv.reader(csv_file)
+            list_from_csv = [row for row in reader]
+        assert list_from_csv == expected
+
+        for f in os.listdir('test_output'):
+            os.remove(os.path.join('test_output', f))
+        shutil.rmtree('test_output')
+
+    def test_to_nifti(self):
+        os.makedirs('test_output', exist_ok=True)
+
+        # Check all is saved.
+        self.segmentation.to_nifti(output_directory='test_output',
+                                   base_file_name='test_subject', maps='all')
+        output_files = os.listdir('test_output')
+        assert len(output_files) == 4
+        assert 'test_subject_mask.nii.gz' in output_files
+        assert 'test_subject_left_kidney.nii.gz' in output_files
+        assert 'test_subject_right_kidney.nii.gz' in output_files
+        assert 'test_subject_individual_kidneys.nii.gz' in output_files
+
+        for f in os.listdir('test_output'):
+            os.remove(os.path.join('test_output', f))
+
+        with pytest.raises(ValueError):
+            self.segmentation.to_nifti(output_directory='test_output',
+                                       base_file_name='test_subject',
+                                       maps='not_an_option')
+
+        # Delete 'test_output' folder
+        shutil.rmtree('test_output')
+
+
+if os.path.exists('test_output'):
+    shutil.rmtree('test_output')
