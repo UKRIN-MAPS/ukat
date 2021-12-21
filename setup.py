80b56584a363e37fb7c7806bece0ae299f6159aa
Alex Daniel <alexander.daniel@nottingham.ac.uk>
2021-12-21 13:24:31 +0000

    Changed python requirements to >=3.7

--- setup.py
+++ setup.py
@@ -17,34 +17,21 @@ setup(
     url="https://github.com/UKRIN-MAPS/ukat",
     license="GPL-3.0",
 
-    python_requires='>=3.6, <4',
+    python_requires='>=3.7, <4',
     packages=find_packages(),
     install_requires=requirements,
     include_package_data=True,
 
-    # Classifiers - the purpose is to create a wheel and upload it to PYPI
     classifiers=[
-        # How mature is this project? Common values are
-        #   3 - Alpha
-        #   4 - Beta
-        #   5 - Production/Stable
         'Development Status :: 4 - Beta',
-
-        # Indicate who your project is intended for
         'Intended Audience :: Developers',
         'Intended Audience :: Science/Research',
         'Topic :: Scientific/Engineering',
         'Environment :: Console',
         'Operating System :: OS Independent',
-        'Programming Language :: Python :: 3',
-        # Specify the Python versions you support here. In particular, ensure
-        # that you indicate you support Python 3. These classifiers are *not*
-        # checked by 'pip install'. See instead 'python_requires' below.
-        'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
-
-        # Pick your license as you wish
+        'Programming Language :: Python :: 3.9',
         'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
     ],
 )
