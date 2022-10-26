25bf8c4c4397d71d550187e0c29fe12dd49a9c88
Alex Daniel <alexander.daniel@nottingham.ac.uk>
2022-10-26 13:36:11 +0100

    New version related changes

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open('README.md', encoding='utf-8') as f:
 
 setup(
     name="ukat",
-    version="0.6.2",
+    version="0.6.3",
     description="UKRIN Kidney Analysis Toolbox",
     long_description=long_description,
     long_description_content_type="text/markdown",
