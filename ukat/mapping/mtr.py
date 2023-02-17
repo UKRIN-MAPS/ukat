1d89be14785d4918fd5a87673494de9107af8b56
Alex Daniel <alexander.daniel@nottingham.ac.uk>
2023-02-17 14:24:37 +0000

    Still trying to get mt_on to not be int64...

--- ukat/mapping/mtr.py
+++ ukat/mapping/mtr.py
@@ -62,7 +62,7 @@ class MTR:
         if mask is None:
             self.mask = np.ones(self.shape, dtype=bool)
         else:
-            self.mask = mask.astype(bool)
+            self.mask = mask
         # The assumption is that MT_OFF comes first in `pixel_array`
         self.mt_off = self.pixel_array[..., 0] * self.mask
         # The assumption is that MT_ON comes second in `pixel_array`
@@ -94,15 +94,18 @@ class MTR:
             for result in maps:
                 if result == 'mtr' or result == 'mtr_map':
                     mtr_nifti = nib.Nifti1Image(self.mtr_map,
-                                                affine=self.affine)
+                                                affine=self.affine,
+                                                dtype=float)
                     nib.save(mtr_nifti, base_path + '_mtr_map.nii.gz')
                 elif result == 'mt_on':
                     mt_on_nifti = nib.Nifti1Image(self.mt_on,
-                                                  affine=self.affine)
+                                                  affine=self.affine,
+                                                  dtype=float)
                     nib.save(mt_on_nifti, base_path + '_mt_on.nii.gz')
                 elif result == 'mt_off':
                     mt_off_nifti = nib.Nifti1Image(self.mt_off,
-                                                   affine=self.affine)
+                                                   affine=self.affine,
+                                                   dtype=float)
                     nib.save(mt_off_nifti, base_path + '_mt_off.nii.gz')
                 elif result == 'mask':
                     mask_nifti = nib.Nifti1Image(self.mask.astype(np.uint16),
