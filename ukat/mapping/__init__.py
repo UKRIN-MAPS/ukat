849998ba0ac50c894f65c46ae76e28ab6842b8c1
Alex Daniel <alexander.daniel@nottingham.ac.uk>
2023-08-16 16:37:19 +0100

    Sanity check that removing numba stops the seg fault

--- ukat/mapping/__init__.py
+++ ukat/mapping/__init__.py
@@ -4,5 +4,5 @@ from .diffusion import ADC, DTI
 from .mtr import MTR
 from .t1 import T1
 from .t2 import T2
-from .t2_stimfit import StimFitModel, T2StimFit
+# from .t2_stimfit import StimFitModel, T2StimFit
 from .t2star import T2Star
