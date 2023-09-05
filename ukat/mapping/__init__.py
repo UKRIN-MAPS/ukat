99657fe9da4c80258d35203b33dddbb6455f43aa
Alex Daniel <alexander.daniel@nottingham.ac.uk>
2023-09-05 14:42:01 +0100

    Commenting out T2 and T2 tests as seg fault seems to occur there

--- ukat/mapping/__init__.py
+++ ukat/mapping/__init__.py
@@ -3,6 +3,6 @@ from .b0 import B0
 from .diffusion import ADC, DTI
 from .mtr import MTR
 from .t1 import T1
-from .t2 import T2
+# from .t2 import T2
 # from .t2_stimfit import StimFitModel, T2StimFit
 from .t2star import T2Star
